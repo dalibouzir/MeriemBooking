faf651698fd0f9aa4a6ff5d4e14d1be45b7b24b7
mohamedali bouzir <bouzirdali@gmail.com>
2025-08-25 21:37:26 +0100

    Fix for Vercel10

--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -1,17 +1,24 @@
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
-import DownloadClient from './DownloadClient';
+import FreeCallClient from './FreeCallClient';
 
 type SearchParams = Record<string, string | string[] | undefined>;
 
 export default async function Page({
   searchParams,
 }: {
-  // Next 15 can type this as a Promise; accept that and await it.
+  // Next 15 sometimes types this as a Promise
   searchParams?: Promise<SearchParams>;
 }) {
   const sp = (await searchParams) ?? {};
-  const product = typeof sp.product === 'string' ? sp.product : '';
-  return <DownloadClient initialProduct={product} />;
+  // accept ?token=... or ?code=...
+  const token =
+    typeof sp.token === 'string'
+      ? sp.token
+      : typeof sp.code === 'string'
+      ? sp.code
+      : '';
+
+  return <FreeCallClient initialToken={token} />;
 }
