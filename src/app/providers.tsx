7f971b91201ca4a39648baf7d025e70df19b5bfe
mohamedali bouzir <bouzirdali@gmail.com>
2025-09-15 17:57:16 +0100

    Calender2

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,16 @@
 "use client"
 
 import type { ReactNode } from 'react'
+import { Suspense } from 'react'
 import { SessionProvider } from 'next-auth/react'
 import RouteProgress from './RouteProgress'
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
     <SessionProvider>
-      <RouteProgress />
+      <Suspense fallback={null}>
+        <RouteProgress />
+      </Suspense>
       {children}
     </SessionProvider>
   )
